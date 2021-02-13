7d09c2a3983cff62629d6cbf53ba027f10999743
Kewarin <kulasatree.jay@kmutt.ac.th>
2021-02-13 16:44:40 +0700

    second

--- 62090500444_TTT.js
+++ 62090500444_TTT.js
@@ -1,7 +1,7 @@
 
 const player = 'o';
 const computer = 'x';
-
+ 
 var curplay = player;
 var gametable = [0,1,2,3,4,5,6,7,8];
 var vistable = [0,1,2,3,4,5,6,7,8];
--- 62090500444_char.js
+++ 62090500444_char.js
@@ -1,5 +1,5 @@
 var name=prompt("What is your favorite charactor?")
 var color=prompt("What is the main color of " + name +"?")
-
+ 
 console.log(name + " is your favorite charactor.")
 console.log(name +" is "+ color + ".")
